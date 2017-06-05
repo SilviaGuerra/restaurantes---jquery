ffef9a416698c0dabefa0818c24aa4e7c5a11044
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-06-05 15:02:42 -0500

    arreglo de objetos con lista de restaurantes

--- js/main.js
+++ js/main.js
@@ -9,3 +9,40 @@ function initMap() {
     map: map
   });
 }
+
+var restaurantes = [
+{
+  "nombre": "Cafebreria El Pendulo Roma",
+  "colonia": "Roma Norte",
+  "categoria": "cafeteria",
+  "ubicacion": "19.418280, -99.158578"
+},
+{
+  "nombre": "Delirio Mónica Patiño",
+  "colonia": "Roma Norte",
+  "categoria": "sandwiches",
+  "ubicacion": "19.417041, -99.165436"
+},
+{
+  "nombre": "Contramar",
+  "colonia": "Roma Norte",
+  "categoria": "mariscos",
+  "ubicacion": "19.419995, -99.166209"
+},
+{
+  "nombre": "Le Pain Quotidien",
+  "colonia": "Condesa",
+  "categoria": "cafeteria",
+  "ubicacion": "19.414405, -99.166381"
+},
+{
+  "nombre": "Fresh Mexico",
+  "colonia": "Roma Norte",
+  "categoria": "Tacos",
+  "ubicacion": "19.417801, -99.157798"
+},
+];
+
+var cargarPagina = function(){
+  $("#search").submit(busqueda);
+}
