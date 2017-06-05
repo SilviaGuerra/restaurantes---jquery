e20955725428250d225c6123c9a582eca658d60a
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-06-05 17:07:05 -0500

    filtra el restaurante pero lo imprime 5 veces

--- js/main.js
+++ js/main.js
@@ -40,9 +40,38 @@ var restaurantes = [
   "colonia": "Roma Norte",
   "categoria": "Tacos",
   "ubicacion": "19.417801, -99.157798"
-},
+}
 ];
 
-var cargarPagina = function(){
-  $("#search").submit(busqueda);
+var plantilla = '<div class="col s12 m7">' + '<h5 class="header nombre">_nombre_</h5>' +
+                '<div class="card horizontal">' + '<div class="card-image">' +
+                '<img src="http://lorempixel.com/100/190/nature/6">' + '</div>' +
+                '<div class="card-stacked">' + '<div class="card-content">' +
+                '<p class="direccion">_direccion_</p>' + '<p class="telefono">_telefono</p>'+
+                '</div>' + '</div>' + '</div>' + '</div>';
+
+var cargarPagina = function() {
+  $("#search-form").submit(busqueda);
 }
+
+var busqueda = function(e) {
+  e.preventDefault();
+  var buscar = $("#search").val().toLowerCase();
+  var resultados = restaurantes.filter(function(lugar){
+    return lugar.nombre.toLowerCase().indexOf(buscar) >=0;
+  });
+
+  lugaresFiltrados(resultados);
+}
+
+var lugaresFiltrados = function(restaurantes) {
+  var plantillaFinal = "";
+  restaurantes.forEach(function(lugar){
+    plantillaFinal += plantilla.replace("_nombre_", lugar.nombre).
+                      replace("_direccion", lugar.colonia).
+                      replace("_telefono_", lugar.ubicacion);
+  });
+  $(".comida").html(plantillaFinal);
+}
+
+$(document).ready(cargarPagina);
